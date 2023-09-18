0957185b26e876567eeae3b30fcc8c140434d9ba
Austin <1158680540@qq.com>
2023-09-18 11:10:47 +0800

    新增点击目录进行收缩

--- myAssets/js/showDocuments.js
+++ myAssets/js/showDocuments.js
@@ -27,16 +27,40 @@ function createFileLi(folderName, FileArr, folderPath) {
 }
 
 function createDocuments() {
+    // Documents是对象，DocumentsUl是元素标签
     var DocumentsUl = document.createElement('ul');
     DocumentsUl.id = Documents.folderName;
     DocumentsUl.textContent = Documents.folderName;
-    document.body.appendChild(DocumentsUl);
+
+    document.querySelector(`.documentsDIV`).appendChild(DocumentsUl);
+
     createFolderUl(DocumentsUl, Documents.folderArr);
     createFileLi(Documents.folderName, Documents.fileArr, Documents.folderPath);
 
     console.log(DocumentsUl)
 }
 
+function clickFolder() {
+    // 监听事件监听的是区域，而不是某个单个元素，所以你监听每个ul就意味着
+    // 点击子元素ul也相当于点击父元素ul，从而触发监听事件
+    var parent = document.querySelector('.documentsDIV');
+    console.log(parent)
+    parent.addEventListener('click', function (e) {
+        if (e.target && e.target.nodeName === 'UL') {
+            var children = e.target.children;
+            for (i = 0; i < children.length; i++) {
+                if (children[i].hasAttribute('style')) {
+                    children[i].removeAttribute('style');
+                } else {
+                    children[i].setAttribute('style', 'display: none');
+                }
+            }
+        }
+    });
+}
+
 window.onload = function () {
     createDocuments();
+
+    clickFolder()
 };
